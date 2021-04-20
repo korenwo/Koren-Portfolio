2e1d493e0b5c8eff41b95ade251797d1a93250e7
koren wohl <korenwohl@gmail.com>
2021-04-20 10:02:18 +0300

    save

--- src/StartPage/StartPage.js
+++ src/StartPage/StartPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import me from './me.jpg';
 import './StartPage.css';
 import { Link } from 'react-router-dom';
+import Typewriter from "typewriter-effect";
 
 function StartPage () {
     return (
@@ -9,40 +10,43 @@ function StartPage () {
             <h1 className="name">KOREN WOHL</h1>
             <img src={me} className="me" alt="myPhoto" />
             <div className="about">
-                Welcome! I am
-
-                
-    Web Developer with advanced skills in Front End Development
+                Welcome! I am 
+                Web Developer with advanced skills in Front End Development
     
-            <div className="onMe">
+                <div className="onMe">
 
-    <li>Passionate about perfect user experience.</li> 
+                    <li>Passionate about perfect user experience.</li> 
 
-    <li> Experienced with both server and client web development, with understanding of performance, security and maintainability.</li>
+                    <li> Experienced with both server and client web development, with understanding of performance, security and maintainability.</li>
 
-    <li> Self learner of new technologies and software development methodologies.</li>
+                    <li> Self learner of new technologies and software development methodologies.</li>
 
-    <div>Here are a few technologies I've been working with :</div>
-            
-           
-                <li>HTML</li> 
-                <li>(S)css</li>
-                <li>JavaScript (ES6+)</li>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>MongoDB</li>
-                <li>RestApi</li>
-            </div>
-            </div>
+                    <div className="here">Here are a few technologies I've been working with :</div>
+                
+                    <Typewriter
+onInit={(typewriter) => {
+    typewriter.typeString("HTML,").pauseFor(2000).start()
+    typewriter.typeString("S(CSS),").pauseFor(2000).start()
+    typewriter.typeString("JavaScript (ES6+),").pauseFor(2000).start()
+    typewriter.typeString("React,").pauseFor(2000).start()
+    typewriter.typeString("Node.js,").pauseFor(1500).start()
+    typewriter.typeString("MongoDB ").pauseFor(1500).start()
+    typewriter.typeString("And more...").pauseFor(1500).start()
+   
+}}
 
-            <Link to="/MyProjects">
-                <button className="projects">My-Projects</button>
-            </Link>
-            <Link to="/Contact">
-                <button className="contact">Contact</button>
-            </Link>
+/>
+                    <Link to="/MyProjects">
+                        <li className="projects">My-Projects</li>
+                    </Link>
+                    <Link to="/Contact">
+                        <li className="contact">Contact</li>
+                    </Link>
+                </div>
+            </div>           
         </div>
     );
 }
 
 export default StartPage;
+
